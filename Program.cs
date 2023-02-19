e55625b885bfa1b98823129413a65de0858eab69
Pavel Grigorev <pavel.grigorev.1990@mail.ru>
2023-02-19 12:33:37 +0300

    added a file with the solution of homework

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string[] array = {"new", "1819", "stpr", "192", "dog", "1254", "progect", "M12"};
+
+string[] ShortArray(string[] array)
+{
+    string[] shortArray = new string[array.Length];
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            shortArray[count] = array[i];
+            count++;
+        }
+    }
+    return shortArray;
+}
+
+void ShowArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+ShowArray(ShortArray(array));
